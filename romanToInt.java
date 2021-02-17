4e1ee4a67c2a8f613579c8040ffea2f37fa87241
Amy Hua <aymay98@gmail.com>
2021-02-17 11:30:52 -0600

    romanToInt updated.

--- romanToInt.java
+++ romanToInt.java
@@ -1,25 +1,25 @@
 class Solution {
     public int romanToInt(String s) {
-        int[] intArray = new int[s.length()];
+        int[] values = new int[s.length()]; 
         for (int i = 0; i < s.length(); i++){
             switch(s.charAt(i)){
-                case 'M': intArray[i] = 1000; break;
-                case 'D': intArray[i] = 500; break;
-                case 'C': intArray[i] = 100; break;
-                case 'L': intArray[i] = 50; break;
-                case 'X': intArray[i] = 10; break;
-                case 'V': intArray[i] = 5; break;
-                case 'I': intArray[i] = 1; break;
+                case 'M': values[i] = 1000; break;
+                case 'D': values[i] = 500; break;
+                case 'C': values[i] = 100; break;
+                case 'L': values[i] = 50; break;
+                case 'X': values[i] = 10; break;
+                case 'V': values[i] = 5; break;
+                case 'I': values[i] = 1; break;
             }
         }
         int sum = 0;
-        for(int i = 0; i < intArray.length - 1; i++){
-            if (intArray[i] < intArray[i + 1]){
-                sum -= intArray[i];
+        for (int i = 0; i < values.length - 1; i++){
+            if (values[i] < values[i + 1]){
+                sum-= values[i];
             }else{
-                sum += intArray[i];
+                sum+= values[i];
             }
         }
-        return sum + intArray[intArray.length - 1];
+        return sum + values[values.length - 1];
     }
 }
