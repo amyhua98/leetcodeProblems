398baa05d1435f061f127adac266b9e7e7776964
Amy Hua <aymay98@gmail.com>
2021-03-09 00:03:10 -0600

    Improve reverseInt

--- reverseInteger.java
+++ reverseInteger.java
@@ -1,13 +1,13 @@
 class Solution {
     public int reverse(int x) {
-        int reversed = 0;
-        while (x != 0){
-            int digit = x % 10;
-            x /= 10;
-            if (reversed > Integer.MAX_VALUE/10 || (reversed == Integer.MAX_VALUE / 10 && digit > 7)) return 0;
-            if (reversed < Integer.MIN_VALUE/10 || (reversed == Integer.MIN_VALUE / 10 && digit < -8)) return 0;
-            reversed = reversed * 10 + digit;
-        }
-        return reversed;
+        if(x<0)
+            return false;
+            int reverse=0;
+            int temp=x;
+            while(temp!=0){
+                reverse=reverse*10+temp%10;
+                temp/=10;
+            }
+                return reverse==x;
     }
 }
