6a5670dce2e10534c848ac092574935becfa229d
Amy Hua <aymay98@gmail.com>
2021-03-17 14:26:36 -0500

    update

--- searchInsertPosition.java
+++ searchInsertPosition.java
@@ -1,15 +1,17 @@
+//binary search
+
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int low = 0;
-        int high = nums.length-1;
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            if(nums[mid] == target){
+        int high = nums.length - 1;
+        while (low <= high){
+            int mid = high - low / 2;
+            if (nums[mid] == target){
                 return mid;
-            }else if (nums[mid] < target){
-                low = mid + 1;
-            }else{
+            }else if(nums[mid] > target){
                 high = mid - 1;
+            }else {
+                low = mid + 1;
             }
         }
         return low;
