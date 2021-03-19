a8f388bf80636ccba2be43e1e92adfec9e18b3f6
Amy Hua <aymay98@gmail.com>
2021-03-19 11:18:04 -0500

    Better answer

--- linkedListCycle.java
+++ linkedListCycle.java
@@ -11,17 +11,21 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head==null)
-            return false;
-        ListNode slow=head;
-        ListNode fast=head;
-        while(fast !=null && fast.next !=null )
-        {
-            slow=slow.next;
-            fast=fast.next.next;
-            if(slow==fast)
-                return true;
+        if (head == null) return false;
+        
+        ListNode slow = head;
+        ListNode fast = head.next;
+        
+        while(slow != fast){
+            
+            if (fast == null || fast. next == null){
+                return false;
+            }
+            
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return false;
+        
+        return true;
     }
 }
