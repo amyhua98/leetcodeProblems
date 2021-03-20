e1018d9160f1e57b3b7f340566f11ce612cc6ca4
Amy Hua <aymay98@gmail.com>
2021-03-20 17:04:40 -0500

    Easier solution to comprehend

--- mergeSortedArray.java
+++ mergeSortedArray.java
@@ -1,15 +1,8 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int indx = m+n-1; 
-		m = m-1;
-		n = n-1;
-		while(m>=0 && n>=0){
-			nums1[indx--] = nums1[m] > nums2[n] ? nums1[m--] :  nums2[n--];
-		}
-
-		if(n>=0){
-			while(n>=0)
-				nums1[indx--] = nums2[n--];
-		}    
+        for (int i = 0; i < nums2.length; i++) {
+            nums1[m+i] = nums2[i];
+        }
+        Arrays.sort(nums1);    
     }
 }
