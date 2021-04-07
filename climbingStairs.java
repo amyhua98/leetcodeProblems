65c4d222845b4db3b94ea8c58dc5105cb0f37308
Amy Hua <aymay98@gmail.com>
2021-04-07 16:50:24 -0500

    climbingStairs updated

--- climbingStairs.java
+++ climbingStairs.java
@@ -1,21 +1,18 @@
-//recursion with memoization
+
+//like fibonacci
 
 public class Solution {
     public int climbStairs(int n) {
-        int memo[] = new int[n + 1];
-        return climb_Stairs(0, n, memo);
-    }
-    public int climb_Stairs(int i, int n, int memo[]) {
-        if (i > n) {
-            return 0;
-        }
-        if (i == n) {
+        if (n == 1) {
             return 1;
         }
-        if (memo[i] > 0) {
-            return memo[i];
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
         }
-        memo[i] = climb_Stairs(i + 1, n, memo) + climb_Stairs(i + 2, n, memo);
-        return memo[i];
+        return second;
     }
 }
