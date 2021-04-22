77ea1879ae3f319807d810bfa03ffeaf6b00761d
Amy Hua <aymay98@gmail.com>
2021-04-22 10:00:42 -0500

    verifyAlienDict updated

--- verifyAlienDict.java
+++ verifyAlienDict.java
@@ -1,24 +1,35 @@
 class Solution {
+    public int[] charMap;
     public boolean isAlienSorted(String[] words, String order) {
-        int[] orderMap = new int[26];
+        charMap = new int[26];
         for (int i = 0; i < order.length(); i++){
-            orderMap[order.charAt(i) - 'a'] = i;
+            charMap[order.charAt(i) - 'a'] = i;
         }
-
-        for (int i = 0; i < words.length - 1; i++) {
-
-            for (int j = 0; j < words[i].length(); j++) {
-                if (j >= words[i + 1].length()) return false;
-
-                if (words[i].charAt(j) != words[i + 1].charAt(j)) {
-                    int currentWordChar = words[i].charAt(j) - 'a';
-                    int nextWordChar = words[i + 1].charAt(j) - 'a';
-                    if (orderMap[currentWordChar] > orderMap[nextWordChar]) return false;
-                    else break;
-                }
+        for(int i = 1; i < words.length; i++){
+            if (compare(words[i-1],words[i]) > 0){
+                return false;
             }
         }
-
         return true;
+    }
+    
+    public int compare(String word1, String word2){
+        int i = 0;
+        int j = 0;
+        int compareVal = 0;
+        
+        //check if it is the same letters
+        while (i < word1.length() && j < word2.length() && compareVal == 0){
+            compareVal = charMap[word1.charAt(i)-'a'] - charMap[word2.charAt(j) - 'a'];
+            
+            i++;
+            j++;
+        }
+        
+        if (compareVal == 0){
+            return word1.length() - word2.length();
+        }else{
+            return compareVal;
+        }
     }
 }
