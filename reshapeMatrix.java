8b396ec6f825998f7b2cf0086c8e1bee6ab1817e
Amy Hua <aymay98@gmail.com>
2021-04-29 12:19:33 -0500

    Update reshapeMatrix

--- reshapeMatrix.java
+++ reshapeMatrix.java
@@ -1,23 +1,23 @@
 class Solution {
     public int[][] matrixReshape(int[][] nums, int r, int c) {
         int rows = nums.length;
-        int columns = nums[0].length;
+        int cols = nums[0].length;
         
-        if((rows * columns) != r * c){
+        if ((rows * cols) != (r * c)){
             return nums;
         }
         
         int[][] output = new int[r][c];
-        int rowNum = 0;
-        int columnNum = 0;
+        int rowIndex = 0;
+        int colIndex = 0;
         
-        for(int i = 0; i < rows; i++){
-            for (int j = 0; j < columns; j++){
-                output[rowNum][columnNum] = nums[i][j];
-                columnNum++;
-                if (columnNum == c){
-                    columnNum = 0;
-                    rowNum++;
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < cols; j++){
+                output[rowIndex][colIndex] = nums[i][j];
+                colIndex++;
+                if(colIndex == c){
+                    colIndex = 0;
+                    rowIndex++;
                 }
             }
         }
