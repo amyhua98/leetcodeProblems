27b135d7c1a78f8d2fcdc9689fd70bd9c9bc7103
Amy Hua <aymay98@gmail.com>
2021-05-05 12:41:43 -0500

    invertTree finished

---
+++ invertTree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+        
+        TreeNode right = invertTree(root.right);
+        TreeNode left = invertTree(root.left);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
+
+
+// 1. check if root is null
+// 2. new node
+// 3. start at root node
+// 4. for every root.left, add a root.right with root.left.val
+// 5. for every root.right, add a root.left with root.right.val
